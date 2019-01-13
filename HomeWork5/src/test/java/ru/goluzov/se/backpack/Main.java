7cf210204cfb8bc2ccef58efad5536e9070b1153
Alecs <goluzov_a@mail.ru>
2019-01-13 14:22:01 +0300

    HomeWork5

---
+++ HomeWork5/src/test/java/ru/goluzov/se/backpack/Main.java
@@ -0,0 +1,26 @@
+package ru.goluzov.se.backpack;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Item> list = new LinkedList<>();
+        list.add(new Item("Книга",   1, 600));
+        list.add(new Item("Бинокль", 2, 5000));
+        list.add(new Item("Аптечка", 4, 1500));
+        list.add(new Item("Ноутбук", 2, 40000));
+        list.add(new Item("Котелок", 1, 500));
+
+
+        Backpack backpack = new Backpack(5);
+        backpack.calcBestSet(list);
+        list = backpack.getBestSet();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).name + " " + list.get(i).price);
+        }
+        System.out.println("Total best price is " + backpack.getMaxPrice());
+        System.out.println("Count: " + backpack.count);
+    }
+
+}
