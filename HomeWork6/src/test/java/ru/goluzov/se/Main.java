1c40427385941a2d49f42d7cbb346e94e741c695
Alecs <goluzov_a@mail.ru>
2019-01-17 22:31:56 +0300

    HomeWork6

---
+++ HomeWork6/src/test/java/ru/goluzov/se/Main.java
@@ -0,0 +1,37 @@
+package ru.goluzov.se;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int QUANTITY_TREE = 20;
+        List<Three> arrTree = new ArrayList<Three>();
+        int balanced=0;
+        int nobalanced = 0;
+
+
+        for (int i = 0; i < QUANTITY_TREE; i++) {
+            Three tree = new Three();
+            tree.generateTree();
+            arrTree.add(tree);
+        }
+
+        for (Three tree : arrTree) {
+            tree.printTree(tree.getRoot(), 0);
+            System.out.println("---------------------");
+            if (tree.isBalanced(tree.getRoot())) {
+                balanced += 1;
+            } else nobalanced += 1;
+
+        }
+        System.out.println("Не сбалансированных - "+ (nobalanced / (balanced + nobalanced) * 100)+"%");
+
+        Three tree = new Three();
+        tree.generateTree();
+        tree.printTree(tree.getRoot(),0);
+        System.out.println(tree.isBalanced(tree.getRoot()));
+    }
+
+}
